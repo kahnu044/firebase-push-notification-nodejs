cf63971018d7469b078a704e76b566cdf7445b8c
Kahnu Charan Swain <djkanha044@gmail.com>
2023-08-25 10:35:53 +0530

    Cors and body parser Added

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 
+// CORS setup for all requests
+const cors = require('cors');
+app.use(cors());
+
+// Parse JSON and URL-encoded form data
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded form data
 
 
 app.use('/', (req, res) => {
